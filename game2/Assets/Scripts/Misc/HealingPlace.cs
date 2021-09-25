fd116f160b416fe133e1a15475b5633922ab7c6e
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2021-09-25 16:14:38 +0200

    Add healing place

---
+++ game2/Assets/Scripts/Misc/HealingPlace.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPlace : MonoBehaviour
+{
+    [SerializeField]
+    private float _healDelay;
+    public HealthBar playerHealthBar;
+    public IntReference playerCurrentHealth;
+    public IntReference playerMaxHP;
+    private bool _isHealing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (playerCurrentHealth.value < playerMaxHP.value) StartCoroutine(HealPlayerCor());
+    }
+
+    IEnumerator HealPlayerCor()
+    {
+        if (_isHealing) yield break;
+        _isHealing = true;
+        playerCurrentHealth.value += 1;
+        playerHealthBar.SetHealth(playerCurrentHealth.value);
+        yield return new WaitForSeconds(_healDelay);
+        _isHealing = false;
+    }
+}
