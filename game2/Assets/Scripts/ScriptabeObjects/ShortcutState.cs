721c84cd364b7c4ca17c2bc411813c1f6eadfd13
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2023-06-01 21:06:08 +0200

    Fix shortcut bug

--- game2/Assets/Scripts/ScriptabeObjects/ShortcutState.cs
+++ game2/Assets/Scripts/ScriptabeObjects/ShortcutState.cs
@@ -1,4 +1,5 @@
 using Gamekit2D;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -13,9 +14,11 @@ public class ShortcutState : ScriptableObject
     [SerializeField] private string id;
     [SerializeField] public bool IsUnlocked;
 
-    private bool isInitalised = false;
+    [SerializeField] public bool IsInitalised { get { return isInitalised; } private set { isInitalised = value; } }
+    [SerializeField] private bool isInitalised;
     private void Awake()
     {
+        Debug.Log(IsInitalised);
         Debug.Log("My ID is: " + Id);
 #if UNITY_EDITOR
 
