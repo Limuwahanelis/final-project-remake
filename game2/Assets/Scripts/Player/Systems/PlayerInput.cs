44dd2b518344d583bb52dc0b780ce7a0deb7115b
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2023-06-06 12:51:11 +0200

    Import new unity input

--- game2/Assets/Scripts/Player/Systems/PlayerInput.cs
+++ game2/Assets/Scripts/Player/Systems/PlayerInput.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField]
-    private Player _player;
+    [SerializeField] Player _player;
+    [SerializeField] InputActionAsset controls;
     private PlayerInteract _playerInteract;
     public GameObject pauseMenu;
     public GameObject darkPanel;
+    private bool isDownArrowPressed;
+    private float direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,51 +28,50 @@ public class PlayerInput : MonoBehaviour
             if (!_player.isGamePaused.value)
             {
                 //if (Input.GetKeyDown(KeyCode.Escape)) SetPause(_player.isGamePaused.value);
-                float direction = Input.GetAxisRaw("Horizontal");
-                Move(direction);
-                if (Input.GetButtonDown("Attack"))
-                {
-                    if(Input.GetKey(KeyCode.DownArrow)) DropBomb();
-                    else Attack();
-                }
-                if (Input.GetButtonDown("Jump"))
-                {
+                _player.currentState.Move(direction);
+                //float direction = Input.GetAxisRaw("Horizontal");
+                //Move(direction);
+                //if (Input.GetButtonDown("Attack"))
+                //{
+                //    if(Input.GetKey(KeyCode.DownArrow)) DropBomb();
+                //    else Attack();
+                //}
+                //if (Input.GetButtonDown("Jump"))
+                //{
 
-                    if (direction * _player.mainBody.transform.localScale.x > 0 && Input.GetKey(KeyCode.DownArrow)) Slide();
-                    else Jump();
-                }
-                if (Input.GetButtonDown("Interact")) Interact();
+                //    if (direction * _player.mainBody.transform.localScale.x > 0 && Input.GetKey(KeyCode.DownArrow)) Slide();
+                //    else Jump();
+                //}
+                //if (Input.GetButtonDown("Interact")) Interact();
 
             }
         }
     }
-
     private void Move(float direction)
     {
         _player.currentState.Move(direction);
     }
-    private void Attack()
+    void OnJump(InputValue value)
     {
-
-        _player.currentState.Attack();
-
+        if (direction * _player.mainBody.transform.localScale.x > 0 && isDownArrowPressed) _player.currentState.Slide();
+        else _player.currentState.Jump();
     }
-    private void Jump()
+    void OnVertical(InputValue value)
     {
-        _player.currentState.Jump();
-    }
-
-    private void Slide()
-    {
-        _player.currentState.Slide();
+        direction=value.Get<float>();
     }
 
-    public void DropBomb()
+    private void OnAttack(InputValue value)
     {
-        _player.currentState.DropBomb();
+        if(isDownArrowPressed) _player.currentState.DropBomb();
+        else _player.currentState.Attack();
     }
-    
-    private void Interact()
+
+    private void OnDownArrowModifier(InputValue value)
+    {
+        isDownArrowPressed = value.Get<float>()==1?true:false;
+    }
+    private void OnInteract(InputValue value)
     {
         _playerInteract.InteractWithObject();
     }
