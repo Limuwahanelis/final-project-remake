8897c09b8fb276b853718627fcc61da42447d696
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2023-06-07 16:57:48 +0200

    Take control from player when changing scenes

--- game2/Assets/Scripts/Utility/SceneTransition.cs
+++ game2/Assets/Scripts/Utility/SceneTransition.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
@@ -15,17 +16,23 @@ public class SceneTransition : MonoBehaviour
     [SerializeField] Transform _playerSpawnPos;
     [SerializeField] GameObject _player;
     [SerializeField] Animator _anim;
+    [SerializeField] InputActionAsset _playerControls;
+    [SerializeField] InputActionAsset _menuControls;
     private void Start()
     {
         if(SceneTransitionManager.tagToTeleportPlayer == _transitionTag)
         {
             _anim.SetTrigger("FadeIn");
             _player.transform.position = _playerSpawnPos.position;
+            _playerControls.Enable();
+            _menuControls.Enable();
         }
     }
 
     public void Load()
     {
+        _playerControls.Disable();
+        _menuControls.Disable();
         StartCoroutine(TransitionCor());
     }
     IEnumerator TransitionCor()
