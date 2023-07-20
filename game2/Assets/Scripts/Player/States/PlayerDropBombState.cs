02d3c49843ae5e17a3823e6d07b18a7cd0ccdf5f
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2023-07-20 14:34:11 +0200

    Fix PlayerDropBombState

--- game2/Assets/Scripts/Player/States/PlayerDropBombState.cs
+++ game2/Assets/Scripts/Player/States/PlayerDropBombState.cs
@@ -6,14 +6,11 @@ public class PlayerDropBombState : PlayerState
 {
     public PlayerDropBombState(PlayerContext playerContext) : base(playerContext)
     {
-        if (_player.isAttacking) return;
-        _player.isAttacking = true;
-        _player.anim.PlayAnimation("Drop Bomb");
-        _player.StartCoroutine(_player.WaitAndExecuteFunction(_player.anim.GetAnimationLength("Drop Bomb"), () =>
+        _playerContext.anim.PlayAnimation("Drop Bomb");
+        _playerContext.corutineHolder.StartCoroutine(_playerContext.WaitAndExecuteFunction(_playerContext.anim.GetAnimationLength("Drop Bomb"), () =>
         {
-            _player.playerCombat.SpawnBomb();
-            _player.ChangeState(new PlayerNormalState(playerContext));
-            _player.isAttacking = false;
+            _playerContext.playerCombat.SpawnBomb();
+            _playerContext.ChangeState(new PlayerNormalState(playerContext));
         }));
     }
 
