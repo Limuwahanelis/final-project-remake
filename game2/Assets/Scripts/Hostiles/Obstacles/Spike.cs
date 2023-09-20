34f58f4d6395ea6dfa6e1fb2013829ae408b0816
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2023-09-20 14:48:54 +0200

    increase spike damage

--- game2/Assets/Scripts/Hostiles/Obstacles/Spike.cs
+++ game2/Assets/Scripts/Hostiles/Obstacles/Spike.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    int dmg = 4;
+    int dmg = 16;
     private void OnCollisionStay2D(Collision2D collision)
     {
         IDamagable player = collision.transform.GetComponent<PlayerHealthSystem>();
